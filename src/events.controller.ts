7a2242d96e7bae841bfdd86046d375b96e428104
Sruba Mohapatra <sruba.mohapatra@publicissapient.com>
2024-02-07 11:00:44 +0530

    Added mapped types library

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,25 +1,50 @@
-import { Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateEventDTO } from './create-event.dto';
 
 @Controller('/events')
 export class EventsController {
-    @Get()
-    findAll() { }
+  data = [
+    {
+      id: 2,
+      name: 'john',
+    },
+    {
+      id: 1,
+      name: 'smith',
+    },
+  ];
+  @Get()
+  findAll() {
+    return this.data;
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id) {
-        return id;
-    }
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return this.data.find((el) => el.id === +id) ?? "Event doesn't exist";
+  }
 
-    @Post()
-    create() { }
+  @Post()
+  create(@Body() input: CreateEventDTO) {
+    return input;
+  }
 
-    @Patch(':id')
-    update(@Param('id') id) {
-        return "patch " + id;
-    }
+  @Patch(':id')
+  update(@Param('id') id, @Body() input) {
+    return input;
+  }
 
-    @Delete(':id')
-    delete(@Param('id') id) {
-        return "delete " + id;
-    }
+  @Delete(':id')
+  @HttpCode(204)
+  delete(@Param('id') id) {
+    return 'delete ' + id;
+  }
 }
