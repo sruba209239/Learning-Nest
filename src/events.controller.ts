dd3bcc990f3f2e560e561d360daf4bc94af278b4
Sruba Mohapatra <sruba.mohapatra@publicissapient.com>
2024-02-08 22:50:40 +0530

    added repository mapping to events table for CRUD ops

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -6,45 +6,56 @@ import {
   HttpCode,
   Param,
   Patch,
-  Post,
+  Post
 } from '@nestjs/common';
 import { CreateEventDTO } from './create-event.dto';
+import { UpdateEventDTO } from './update-event.dto';
+import { Event } from './event.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller('/events')
 export class EventsController {
-  data = [
-    {
-      id: 2,
-      name: 'john',
-    },
-    {
-      id: 1,
-      name: 'smith',
-    },
-  ];
+  constructor(
+    @InjectRepository(Event)
+    private repository: Repository<Event>
+  ) {}
+
   @Get()
-  findAll() {
-    return this.data;
+  async findAll() {
+    return await this.repository.find();
   }
 
   @Get(':id')
-  findOne(@Param('id') id) {
-    return this.data.find((el) => el.id === +id) ?? "Event doesn't exist";
+  async findOne(@Param('id') id) {
+    return (await this.repository.findBy({ id: id })) ?? "Event doesn't exist";
   }
 
   @Post()
   create(@Body() input: CreateEventDTO) {
+    this.repository.save({
+      ...input,
+      when: new Date(input.when)
+    });
     return input;
   }
 
   @Patch(':id')
-  update(@Param('id') id, @Body() input) {
-    return input;
+  async update(@Param('id') id, @Body() input: UpdateEventDTO) {
+    let rowToBeUpdated = await this.repository.findOneBy({ id: id });
+    rowToBeUpdated = {
+      ...rowToBeUpdated,
+      ...input,
+      when: input.when ? new Date(input.when) : rowToBeUpdated.when
+    };
+    this.repository.save(rowToBeUpdated);
+    return rowToBeUpdated;
   }
 
   @Delete(':id')
   @HttpCode(204)
-  delete(@Param('id') id) {
-    return 'delete ' + id;
+  async delete(@Param('id') id) {
+    const event = await this.repository.findBy({ id: id });
+    await this.repository.remove(event);
   }
 }
