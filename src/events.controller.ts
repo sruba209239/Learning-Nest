b4a73be2bf98d368c328220684220297d1ccede7
Sruba Mohapatra <sruba.mohapatra@publicissapient.com>
2024-02-20 14:58:09 +0530

    added repository to controller and replaced methods

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   HttpCode,
   Param,
+  ParseIntPipe,
   Patch,
   Post
 } from '@nestjs/common';
@@ -44,7 +45,8 @@ export class EventsController {
   }
 
   @Get(':id')
-  async findOne(@Param('id') id) {
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    console.log(typeof id);
     return (await this.repository.findBy({ id: id })) ?? "Event doesn't exist";
   }
 
